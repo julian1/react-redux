998650752337cfd6ff3a68c9d97bebeaea9b7b76
julian1 <julian.atkinson71@gmail.com>
2015-10-11 17:42:02 +1100

    ok doing a query

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var server = require('http').createServer()
   , express = require('express')
   , app = express()
   , port = 8081
+
 ;
 
 var path = require('path');
-var fs = require('fs');
+// var fs = require('fs');
+var pg = require('pg');
+
 
 // https://github.com/rackt/react-router/blob/master/docs/guides/basics/Histories.md
 
@@ -22,6 +25,32 @@ app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
 app.get('/myendpoint', function(request, response){
   
   console.log(request.query); 
+
+  // we want a connection pool ...
+  var conString = "postgres://meteo:meteo@127.0.0.1/contr_vocab_db?ssl=true";
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    var query  = 'select json_agg( row_to_json(a)) as result from ( select * from responsible_party) as a';
+
+    // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+
+    client.query( query , function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+      console.log(result.rows[0].result);
+      //output: 1
+
+      // ok, so we have to ensure it's json and send it...
+    });
+  });
+
 
   response.send( { whoot: "123" });
 })
