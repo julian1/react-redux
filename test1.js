0e8a1ed8a32baa0d79949d2577919d8205b8cd6b
julian1 <julian.atkinson71@gmail.com>
2015-10-18 18:00:24 +1100

    update

--- test1.js
+++ test1.js
@@ -9,7 +9,7 @@ pg.defaults.poolSize = 10;
 // are we going to be text expansion?
 // or argument expansion?
 
-function test1() { 
+function test1( query_ ) { 
 
   var conString = "postgres://meteo:meteo@127.0.0.1/contr_vocab_db?ssl=true";
   pg.connect(conString, (err, client, done) => {
@@ -17,8 +17,8 @@ function test1() {
       console.error('error fetching client from pool', err);
     }
 
-    var query1 = 'select * from responsible_party';
-    var query  = 'select json_agg( row_to_json(a)) as result from ( ' + query1 + ') as a';
+    // var query1 = 'select * from responsible_party';
+    var query = 'select json_agg( row_to_json(a)) as result from ( ' + query_ + ') as a';
 
     client.query( query , function(err, result) {
       // call `done()` to release the client back to the pool
@@ -37,20 +37,55 @@ function test1() {
 
 // so what do we do here... and what are the functions...
 
+let curry = (fn, ...args) => {
+  let _curry = (args) =>
+    args.length < fn.length
+      ? (..._args) => _curry([...args, ..._args])
+      : fn(...args);
 
-// for responsible party
-var t = {
-  name:  'organisation_name',
-  ref_field: 'organisation_id',
-  ref_sub: 'name',          
-  relation: 'organisation' ,
+  return _curry(args);
 };
 
-console.log( ` ${ t.name }` );
+// for responsible party
+const t = {
+  name:  'organisation_name',
+  field: 'organisation_id',
+  for_relation: 'organisation' ,
+  for_sub: 'name',          
+};
+
+const p = {
+  name:  'person_name',
+  field: 'person_id',
+  for_relation: 'person' ,
+  for_sub: 'name',          
+};
+
+
+const m = [ t, p ];
+
+// might be easier
+// ok, can we change this  
+
+//     ${t.for_relation}.${ t.for_sub } as ${ t.name} 
+
+//  join ${t.for_relation} on ${t.for_relation}.id = rp.${ t.field} 
+
+// 
+
+const query = `
+  select 
+    ${ m.map( key => key.for_relation + '.' + key.for_sub + ' as ' + key.name).reduce((l, r) => l + ',\n' +  r)   }
+  from responsible_party rp
+
+    ${ m.map( key => ' join ' + key.for_relation + ' on ' + key.for_relation + '.id = rp. ' + key.field ) .reduce((l, r) => l + '\n' +  r)  }
+`;
+
+console.log( '' + query );
 
 // es6
 
-test1();
+test1( query);
 
 // close pool which will, finish everything
 pg.end();
